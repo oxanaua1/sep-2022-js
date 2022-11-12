690a3170c206fc9815582c410d4e6d1fd1679c1e
OksanaKotsur <oxanaua1@gmail.com>
2022-11-12 18:57:33 +0200

    lesson4_add.FINAL

--- lesson4/additional1/main.js
+++ lesson4/additional1/main.js
@@ -115,45 +115,300 @@
 //     }
 // }
 // console.log(numArr);
-
-// 8. вивести масив в зворотньому порядку.
-let numArray = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
-
-let newNumArray = [];
-
-for (let q = numArray.length - 1, f = 0; q >= 0; q--, f++) {
-
-    newNumArray [f] = numArray[q];
-
-}
-console.log(newNumArray);
-
-
-// 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
-// if (numArr[j]%2){
-//     console.log(numArr[j]);
+//
+// //8. вивести масив в зворотньому порядку.
+//
+// let numArray = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+//
+// let newNumArray = [];
+//
+// for (let q = numArray.length - 1, f = 0; q >= 0; q--, f++) {
+//
+//     newNumArray [f] = numArray[q];
+//
 // }
-
-//
-// - Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
-// - Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
-// - Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
-// - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
-// - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
-// - Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
-//
-// - Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів. Вивести в консоль всі його елементи в циклі.
-// - Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
-// - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
-// - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
-// - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
-// - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+// console.log(newNumArray);
 //
 //
+// //- Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
 //
-// стоврити масив книжок (назва, кількість сторінок, автори , жанри).
-// -знайти наібльшу книжку.
-// - знайти книжку/ки з найбільшою кількістю жанрів
-// - знайти книжку/ки з найдовшою назвою
-// - знайти книжку/ки які писали 2 автори
-// - знайти книжку/ки які писав 1 автор
+// let numbers = [10, -20, 100, 0.5, 10500, 300, 400, 4, 8, 33];
+// for (const number of numbers) {
+//         console.log(number);
+// }
+//
+//
+// //- Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
+//
+// let fruits = ['Apple', 'Banana', 'Apricot', 'Nectarine', 'Oranges', 'Grape', 'Mango', 'Papaya', 'Pineapple', 'Kiwi'];
+// for (const fruit of fruits) {
+//         console.log(fruit);
+// }
+//
+// //- Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+//
+// let array = [10, -20, 'Apple', 'Banana', 100, 'Grape', 0.5, true, 11, false];
+// let i = 0;
+// while (i < array.length) {
+//
+//     console.log(array[i]);
+//     i++;
+// }
+//
+// //- Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
+// let array = [10, -20, 'Apple', 'Banana', 100, 'Grape', 0.5, true, 11, false];
+//
+// for (const element of array) {
+//     if (typeof element === "boolean") {
+//         console.log(element);
+//     }
+// }
+// //- Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
+// let array = [10, -20, 'Apple', 'Banana', 100, 'Grape', 0.5, true, 11, false];
+// for (const element of array) {
+//     if (typeof element === "number") {
+//         console.log(element);
+//     }
+// }
+//
+// //- Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
+// let array = [10, -20, 'Apple', 'Banana', 100, 'Grape', 0.5, true, 11, false];
+// for (const element of array) {
+//     if (typeof element === "string") {
+//         console.log(element);
+//     }
+// }
+//
+// //- Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів.
+// //Вивести в консоль всі його елементи в циклі.
+//
+// let arr = [];
+//
+// for (let i = 0; i < 10; i++) {
+//     if (i <= 3) {
+//         arr[i] = i
+//
+//     } else if (i >= 4 && i <= 7) {
+//         arr[i] = `${i}`
+//
+//     } else arr[i] = i === 8;
+//
+// }
+//
+// console.log(arr);
+//
+// //- Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+//
+// for (let i = 0; i < 10; i++) {
+//
+//     console.log(i);
+//     document.write(`\n ${i}`);
+// }
+//
+//
+// let i = 0;
+// while (i < 10) {
+//
+//     console.log(i);
+//     document.write(`${i}`);
+//
+//     i++;
+// }
+//
+//
+// //- Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+//
+// for (let i = 0; i < 100; i++) {
+//
+//     console.log(i);
+//     document.write(`\n ${i}`);
+// }
+//
+// let i = 0;
+// while (i < 100) {
+//
+//     console.log(i);
+//     document.write(`\n ${i}`);
+//
+//     i++;
+// }
+// //- Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
+//
+// for (let i = 0; i < 100; i+=2) {
+//
+//     console.log(i);
+//     document.write(`\n ${i}`);
+// }
+//
+// //- Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+// for (let i = 0; i < 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//         document.write(`\n ${i}`);
+//     }
+//
+// }
+//
+// //- Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+// for (let i = 0; i < 100; i++) {
+//
+//     if (!(i % 2 === 0)) {
+//         console.log(i);
+//         document.write(`\n ${i}`);
+//     }
+// }
+//
+// //стоврити масив книжок (назва, кількість сторінок, автори , жанри).
+// let books = [
+//     {
+//         name: 'You are the only one',
+//         page: 500,
+//         genre: ['Romance', 'Detective'],
+//         author: [{name: 'Dan White'}]
+//     },
+//     {
+//         name: 'Another world',
+//         page: 600,
+//         genre: ['Romance', 'Detective', 'Fantasy'],
+//         author: [{name: 'Jack Black'}, {name: 'Dan White'}, {name: 'Keven Klone'}],
+//     },
+//     {
+//         name: 'Good night',
+//         page: 100,
+//         genre: ['Thriller'],
+//         author: [{name: 'Ivan Navi'}]
+//     },
+//     {
+//         name: 'At any time',
+//         page: 200,
+//         genre: ['Fantasy', 'Detective', 'Thriller', 'Fiction'],
+//         author: [{name: 'Ivan Navi'}, {name: 'Dan White'}]
+//     }
+//
+// ]
+// console.log(books);
+//
+// // -знайти найбльшу книжку.
+//
+// let pageMax = books[0];
+//
+// for (const book of books) {
+//     if (book.page > pageMax.page) {
+//         pageMax = book
+//     }
+// }
+// console.log(pageMax);
+//
+//
+// // - знайти книжку/ки з найбільшою кількістю жанрів
+//
+// let genreMax = books[0];
+//
+// for (const book of books) {
+//     if (book.genre.length > genreMax.genre.length) {
+//         genreMax = book
+//     }
+//
+// }
+// console.log(genreMax);
+//
+//
+// // - знайти книжку/ки з найдовшою назвою
+// let nameMax = books[0]
+// for (const book of books) {
+//     if (book.name.length > nameMax.name.length) {
+//         nameMax = book
+//     }
+//
+// }
+// console.log(nameMax);
+//
+// // - знайти книжку/ки які писали 2 автори
+//
+// let twoAuthBook = books[0];
+//
+// for (const book of books) {
+//     if (book.author.length === 2) {
+//         twoAuthBook = book
+//
+//     }
+// }
+// console.log(twoAuthBook);
+//
+//
+// // - знайти книжку/ки які писав 1 автор
+//
+// let oneAuthBook = books[0];
+//
+// for (const book of books) {
+//     if (book.author.length === 1) {
+//         oneAuthBook = book
+//
+//     }
+// }
+// console.log(oneAuthBook);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// //---------------------------------------------------------------------------------------------------------------------
+// //(Моє)
+// //- знайти книжкки які писали 2 автори і додати в новий масив
+//
+// // let books1 = [
+// //     {
+// //         name: 'You are the only one',
+// //         page: 500,
+// //         genre: ['Romance', 'Detective'],
+// //         author: [{name: 'Dan White'}, {name: 'Keven Klone'}]
+// //     },
+// //     {
+// //         name: 'Another world',
+// //         page: 600,
+// //         genre: ['Romance', 'Detective', 'Fantasy'],
+// //         author: [{name: 'Jack Black'}, {name: 'Dan White'}, {name: 'Keven Klone'}],
+// //     },
+// //     {
+// //         name: 'Good night',
+// //         page: 100,
+// //         genre: ['Thriller'],
+// //         author: [{name: 'Ivan Navi'}]
+// //     },
+// //     {
+// //         name: 'At any time',
+// //         page: 200,
+// //         genre: ['Fantasy', 'Detective', 'Thriller', 'Fiction'],
+// //         author: [{name: 'Ivan Navi'}, {name: 'Dan White'}]
+// //     }
+// //
+// // ]
+// //
+// // let twoAuthBook1 = [];
+// //
+// // for (const book1 of books1) {
+// //     if (book1.author.length === 2) {
+// //         twoAuthBook1.push(book1)
+// //     }
+// //
+// // }
+// //
+// // console.log(twoAuthBook1);
